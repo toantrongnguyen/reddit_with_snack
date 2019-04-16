0546c72099afd8c4e4e771fdafb4eb3dc60fa180
hoangTien <nguyen.hoang.tien@framgia.com>
2019-04-16 19:26:23 +0700

    Login Page

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VeeValidate, { Validator } from 'vee-validate'
+import dictionary from './lang/dictionary'
+import 'bootstrap/dist/css/bootstrap.css'
 
 Vue.config.productionTip = false
+Validator.localize(dictionary)
+
+Vue.use(VeeValidate)
+
+Validator.extend('password', {
+  getMessage: () => dictionary.en.password.strong_password,
+  validate: value => {
+    const strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})")
+    return strongRegex.test(value)
+  }
+})
 
 new Vue({
   render: h => h(App),
---
+++ client/src/pages/LoginPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class='login-page-display container-fluid'>
+    <div class="row">
+      <authentication-layout></authentication-layout>
+      <brief-description></brief-description>
+    </div>
+  </div>
+</template>
+
+<script>
+import AuthenticationLayout from '@/components/Login/AuthenticationLayout'
+import BriefDescription from '@/components/Login/BriefDescription'
+
+export default {
+  name: 'LoginPage',
+  components: {
+    AuthenticationLayout,
+    BriefDescription,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .login-page-display {
+    /* display: flex; */
+  }
+</style>
