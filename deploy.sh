cc94f3fd204a188126accd18f0dec566b03891e8
toannt181 <nguyen.trong.toan@framgia.com>
2019-04-27 23:45:10 +0700

    Test

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+cd /home/work/reddit_with_snack/
 sudo git pull origin master
 
-sudo docker-compose down
-sudo docker-compose build
-sudo docker-compose up -d
+# sudo docker-compose down
+# sudo docker-compose build
+# sudo docker-compose up -d
